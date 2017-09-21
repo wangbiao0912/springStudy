77a8522ec70fb0a16db69365188813da43acb4ae
wangbiaoJava <229492022@qq.com>
2017-09-21 17:13:46 +0800

    '定时任务开启'

--- SpringBootStudy/src/main/test/java/com/after/SpringbootApplicationTests.java
+++ SpringBootStudy/src/main/test/java/com/after/SpringbootApplicationTests.java
@@ -1,14 +1,22 @@
+import com.fasterxml.jackson.databind.ser.Serializers;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes=Serializers.Base.class)
+@Slf4j
 public class SpringbootApplicationTests {
+
 
 	@Test
 	public void contextLoads() {
+		String test="!!!!";
+		log.info("dddd+ dsa  ${}",test);
 	}
 
 }
